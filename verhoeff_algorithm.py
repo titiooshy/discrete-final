5fc3f725a98923f6b6cb69d65245123b5c3dda3a
Titilayo Oshinowo <titiooshy@gmail.com>
2025-12-08 09:53:38 -0500

    Initial verhoeff_algorithm stuff

---
+++ verhoeff_algorithm.py
@@ -0,0 +1,35 @@
+# Overall Code for Verhoeff Algorithm
+
+# First we will initialize the multiplication table
+mult_table = [
+    (0, 1, 2, 3, 4, 5, 6, 7, 8, 9),
+    (1, 2, 3, 4, 0, 6, 7, 8, 9, 5),
+    (2, 3, 4, 0, 1, 7, 8, 9, 5, 6),
+    (3, 4, 0, 1, 2, 8, 9, 5, 6, 7),
+    (4, 0, 1, 2, 3, 9, 5, 6, 7, 8),
+    (5, 9, 8, 7, 6, 0, 4, 3, 2, 1),
+    (6, 5, 9, 8, 7, 1, 0, 4, 3, 2),
+    (7, 6, 5, 9, 8, 2, 1, 0, 4, 3),
+    (8, 7, 6, 5, 9, 3, 2, 1, 0, 4),
+    (9, 8, 7, 6, 5, 4, 3, 2, 1, 0),
+]
+
+# We will write down the inverse of that number
+inverse = (0, 4, 3, 2, 1, 5, 6, 7, 8, 9)
+
+# We will then write out the permutation table
+perm_table = [
+    (0, 1, 2, 3, 4, 5, 6, 7, 8, 9),
+    (1, 5, 7, 6, 2, 8, 3, 0, 9, 4),
+    (5,),
+    (8,),
+    (9,),
+    (4,),
+    (2,),
+    (7,),
+]
+
+
+# With these finally set up we can write out the function
+def verhoeff(number, validate=True, terse=True, verbose=False):
+    """ """
